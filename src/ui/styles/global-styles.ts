1ab03dad3d72b16f2910f81c86934e4eea896ebe
Christian Ivicevic <mail@christian-ivicevic.com>
2021-01-31 18:24:11 +0100

    fix: add line height fix and box sizing reset to global styles

--- src/ui/styles/global-styles.ts
+++ src/ui/styles/global-styles.ts
@@ -1,6 +1,21 @@
 import { createGlobalStyle } from 'styled-components/macro';
 
 export const GlobalStyles = createGlobalStyle`
+html {
+  // Line height no longer need to be normalized to opinionated values clashing
+  // with our fonts since all common browser now have sane defaults so we
+  // revert the normalization process intentionally.
+  line-height: normal;
+}
+
+// Box sizing reset
+html {
+  box-sizing: border-box;
+}
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
 body {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
